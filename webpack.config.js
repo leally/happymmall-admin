81981d93058e821cc919e35ba04297e1080a26b3
lea <xdipeng@sina.com>
2019-11-27 00:12:59 +0800

    第一次提交分支

---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+/*
+ * @Author: leally Xie 
+ * @Date: 2019-11-25 23:33:02 
+ * @Last Modified by: leally Xie
+ * @Last Modified time: 2019-11-26 23:58:52
+ */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'js/app.js'
+  },
+  devServer: {
+    port: 8086
+  },
+  module: {
+    rules: [
+      // react 文件的处理
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      },
+      // css 文件的处理
+      {
+        test: /\.css$/i,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        }),
+      },
+      // sass 文件的处理
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      // 图片资源的配置
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'resource/[name].[ext]'
+            }
+          }
+        ]
+      },
+      // 处理字体文件
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'resource/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // 处理HTML文件
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    // 处理css文件
+    new ExtractTextPlugin("css/[name].css"),
+    // 提取公共模块
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      filename: 'js/base.js'
+    })
+  ]
+};
